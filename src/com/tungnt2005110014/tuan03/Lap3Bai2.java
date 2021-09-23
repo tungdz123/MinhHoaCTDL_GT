77fda25bbd7b5b685c676e1d4e5a239e55a5b896
tungdz123 <tung.chuot.2508@gmail.com>
2021-09-23 10:03:23 +0700

    bài tập Lap3Bai2

---
+++ src/com/tungnt2005110014/tuan03/Lap3Bai2.java
@@ -0,0 +1,12 @@
+package com.tungnt2005110014.tuan03;
+
+public class Lap3Bai2 {
+    public static void main(String[] args) {
+        for(int i=1; i<=9; i++){
+            for(int j=1; j<=10; j++){
+                System.out.printf("%d x %d = %d", i, j, i*j);
+                System.out.println();
+            }
+        }
+    }
+}
